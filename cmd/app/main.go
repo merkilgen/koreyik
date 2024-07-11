7075659730d439388c4e8854b2638dd1540f1046
serwennn <nassen.d230@gmail.com>
2024-07-11 13:40:06 +0500

    adding chi to the project and a bit more project structure

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Println("The start of the project")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", handler)
+
+	log.Print("Listening server on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("welcome"))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module koreyik
 
 go 1.22.3
+
+require github.com/go-chi/chi/v5 v5.1.0 // indirect
