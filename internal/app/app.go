061b6cd889d0f4e03ec0a5d86606692ad64dd86f
serwennn <nassen.d230@gmail.com>
2024-08-09 23:42:28 +0500

    graceful shutdown

--- internal/app/app.go
+++ internal/app/app.go
@@ -7,6 +7,9 @@ import (
 	"github.com/serwennn/koreyik/internal/server"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func Run() {
@@ -24,9 +27,22 @@ func Run() {
 		w.Write([]byte("Koreyik!"))
 	})
 
-	// TODO: Graceful shutdown
-
 	srv := server.NewServer(cfg, r)
+	go func() {
+		if err := srv.Run(); err != nil {
+			log.Fatalf("Failed to run the server: %s\n", err.Error())
+		}
+	}()
+
 	log.Printf("Server is running on http://%s [ENV: %s]\n", srv.HttpServer.Addr, cfg.Env)
-	log.Fatal(srv.Run())
+
+	// Graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+
+	<-quit
+
+	// TODO: Close the storage
+
+	log.Println("Shutting down the server...")
 }
