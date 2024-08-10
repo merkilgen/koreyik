281fd55c077f3bf72b18d8b76dfdf7dbf95ee3b5
serwennn <nassen.d230@gmail.com>
2024-08-10 11:57:57 +0500

    router initialization moved to internal/network

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,12 +1,11 @@
 package app
 
 import (
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 	"github.com/serwennn/koreyik/internal/config"
+	"github.com/serwennn/koreyik/internal/network"
 	"github.com/serwennn/koreyik/internal/server"
+	"github.com/serwennn/koreyik/internal/storage"
 	"log"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -15,17 +14,10 @@ import (
 func Run() {
 	cfg := config.New()
 
-	// stg := storage.New(cfg.StoragePath)
+	stg := storage.New(cfg.StoragePath)
+	_ = stg // TODO: Use the storage
 
-	r := chi.NewRouter()
-
-	r.Use(middleware.RequestID)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Koreyik!"))
-	})
+	r := network.NewRouter()
 
 	srv := server.NewServer(cfg, r)
 	go func() {
